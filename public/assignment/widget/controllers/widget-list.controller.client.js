59ad7682540d5f61b039993df7db376f1e2f3735
Prasad Tajane <tajane.pr@husky.neu.edu>
2017-07-21 22:22:13 -0400

    Assignment3 part13 - Added widget service start

--- public/assignment/widget/controllers/widget-list.controller.client.js
+++ public/assignment/widget/controllers/widget-list.controller.client.js
@@ -7,7 +7,7 @@
         .module("WamApp")
         .controller("widgetController", widgetController);
 
-    function widgetController($location, $routeParams, $sce) {
+    function widgetController($location, $routeParams, $sce, widgetService) {
 
         var model = this;
 
@@ -17,45 +17,12 @@
 
         model.backToProfile=backToProfile;
         model.backToPageList=backToPageList;
+        model.findWidgetByPageId=findWidgetByPageId;
         model.goToNewWidgetChooser=goToNewWidgetChooser;
 
-        var widgets = [
-            { "_id": "123"
-                , "widgetType": "HEADING"
-                , "pageId": "321"
-                , "size": 2
-                , "text": "GIZMODO"},
-            { "_id": "234"
-                , "widgetType": "HEADING"
-                , "pageId": "321"
-                , "size": 4
-                , "text": "Lorem ipsum"},
-            { "_id": "345"
-                , "widgetType": "IMAGE"
-                , "pageId": "321"
-                , "width": "100%"
-                , "url": "http://lorempixel.com/400/200/"},
-            { "_id": "456"
-                , "widgetType": "HTML"
-                , "pageId": "321"
-                , "text": "<p>Lorem ipsum</p>"},
-            { "_id": "567"
-                , "widgetType": "HEADING"
-                , "pageId": "321"
-                , "size": 4
-                , "text": "Lorem ipsum"},
-            { "_id": "678"
-                , "widgetType": "YOUTUBE"
-                , "pageId": "321"
-                , "width": "100%"
-                , "url": "https://youtu.be/AM2Ivdi9c4E" },
-            { "_id": "789"
-                , "widgetType": "HTML"
-                , "pageId": "321"
-                , "text": "<p>Lorem ipsum</p>"}
-        ];
-
         function init() {
+            model.widgetList = widgetService.findWidgetByPageId(pageId);
+            return model.widgetList;
         }
         init();
 
@@ -69,7 +36,11 @@
             //alert("inside backToPageList from page-new.controller")
             $location.url("/profile/" + userId + "/website/" + websiteId + "/page");
         }
-        
+
+        function findWidgetByPageId()    {
+            return widgetService.findWidgetByPageId(pageId);
+        }
+
         function goToNewWidgetChooser() {
             ///user/:uid/website/:wid/page/:pid/widget/new
             $location.url("/profile/" + userId + "/website/" + websiteId + "/page/" + pageId + "/widget/new");
